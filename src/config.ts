cb13e005d9c8eba7b86b2a8465f5371250382ba3
legolaptop <49568737+legolaptop@users.noreply.github.com>
2026-04-02 22:22:10 -0700

    Add Google OAuth configuration constants

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// src/config.ts
+export const GOOGLE_CLIENT_ID = 'client_secret_1072507978414-bifrk9rra5knrkf6rfpbjahb9174gd1t.apps.googleusercontent.com';
+
+// Required OAuth scopes
+export const GOOGLE_SCOPES = [
+  'https://www.googleapis.com/auth/drive.appdata',
+].join(' ');
